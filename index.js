270e297370b552c6723999dd5904bd50d1124e56
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-07 17:35:25 +0530

    the fetch api

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-const getTodos= (resource) => {
+// const getTodos= (resource) => {
    
-   return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+//    return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', () => {
-        // console.log(request, request.readyState)
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText)
-            resolve(data)
-        } else if (request.readyState === 4) {
-            reject('could not fetch data from server')
-        }
-    })
+//     request.addEventListener('readystatechange', () => {
+//         // console.log(request, request.readyState)
+//         if (request.readyState === 4 && request.status === 200) {
+//             const data = JSON.parse(request.responseText)
+//             resolve(data)
+//         } else if (request.readyState === 4) {
+//             reject('could not fetch data from server')
+//         }
+//     })
     
-    request.open('GET' , resource)
-    request.send();
+//     request.open('GET' , resource)
+//     request.send();
 
-   })
+//    })
    
    
-}
+// }
 
-console.log(1)
-console.log(2)
+// console.log(1)
+// console.log(2)
 
-// getTodos('lugi.json' ,(err, data) => {
-//     console.log(data)
-//     getTodos('mario.json' ,(err, data) => {
-//         console.log(data)
-//     })
-//     getTodos('todos.json' ,(err, data) => {
-//         console.log(data)
-//     })
-// })
+// // getTodos('lugi.json' ,(err, data) => {
+// //     console.log(data)
+// //     getTodos('mario.json' ,(err, data) => {
+// //         console.log(data)
+// //     })
+// //     getTodos('todos.json' ,(err, data) => {
+// //         console.log(data)
+// //     })
+// // })
 
-getTodos('todos.json').then(data => {
-     console.log('promise 1 resolved:', data)
-     return getTodos('lugi.json').then(data => {
-        console.log('promise 2 resolved:', data)
-        return getTodos('mario.json').then(data => {
-            console.log('promise 3 resolved:', data)
-        })
-     })    
+// getTodos('todos.json').then(data => {
+//      console.log('promise 1 resolved:', data)
+//      return getTodos('lugi.json').then(data => {
+//         console.log('promise 2 resolved:', data)
+//         return getTodos('mario.json').then(data => {
+//             console.log('promise 3 resolved:', data)
+//         })
+//      })    
+// }).catch(err => {
+//     console.log('promise rejected:', err)
+// });
+
+
+// console.log(3)
+// console.log(4)
+
+fetch('lugi.json').then((response) => {
+    console.log(response)
+    return response.json() // response.json() is a promise
+}).then(data => {
+    console.log(data)
 }).catch(err => {
-    console.log('promise rejected:', err)
-});
-
-
-console.log(3)
-console.log(4)
-
+    console.log('rejected', err)
+})
