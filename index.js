3eeb5b7c5cf4bcd0159b664c2663db76983cee22
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-07 17:11:20 +0530

    Promises

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-const getTodos= (resource ,callback) => {
+const getTodos= (resource) => {
+   
+   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
         // console.log(request, request.readyState)
         if (request.readyState === 4 && request.status === 200) {
             const data = JSON.parse(request.responseText)
-            callback(undefined, data)
+            resolve(data)
         } else if (request.readyState === 4) {
-            callback('could not fetch data from server', undefined)
+            reject('could not fetch data from server')
         }
     })
     
     request.open('GET' , resource)
     request.send();
+
+   })
+   
+   
 }
 
 console.log(1)
 console.log(2)
 
-getTodos('lugi.json' ,(err, data) => {
-    console.log(data)
-    getTodos('mario.json' ,(err, data) => {
-        console.log(data)
-    })
-    getTodos('todos.json' ,(err, data) => {
-        console.log(data)
-    })
-})
+// getTodos('lugi.json' ,(err, data) => {
+//     console.log(data)
+//     getTodos('mario.json' ,(err, data) => {
+//         console.log(data)
+//     })
+//     getTodos('todos.json' ,(err, data) => {
+//         console.log(data)
+//     })
+// })
+
+getTodos('todos.json').then(data => {
+     console.log('promise resolved:', data)
+}).catch(err => {
+    console.log('promise rejected:', err)
+});
+
 
 console.log(3)
 console.log(4)
+
